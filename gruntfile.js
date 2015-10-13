d673303ef6683ebc03043d3b281d99ae1a2a4477
Dakota Lightning <im@dakotalightning.com>
2015-10-13 14:25:13 -0700

    initial

---
+++ gruntfile.js
@@ -0,0 +1,61 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+
+var sassOptions = {
+    expand: true,
+    cwd: 'scss',
+    src: ["**/*.scss"],
+    dest: "css"
+}
+
+module.exports = function (grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            compact: {
+                options: {
+                    style: 'compact'
+                },
+                files: [
+                  {
+                      expand: sassOptions.expand,
+                      cwd: sassOptions.cwd,
+                      src: sassOptions.src,
+                      dest: sassOptions.dest,
+                      ext: ".css"
+                  }
+                ]
+            },
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: [
+                  {
+                      expand: sassOptions.expand,
+                      cwd: sassOptions.cwd,
+                      src: sassOptions.src,
+                      dest: sassOptions.dest,
+                      ext: ".min.css"
+                  }
+                ]
+            }
+        },
+        watch: {
+            sass: {
+                files: 'scss/**/*.scss',
+                tasks: ["sass"],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', []);
+};
---
+++ readme.txt
@@ -0,0 +1,3 @@
+## Simple project start with static server
+
+    npm install
