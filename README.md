5ba9a8669d003192c88dc295a7540e2a34352bdd
Dakota Lightning <im@dakotalightning.com>
2016-02-02 14:29:57 -0800

    new

---
+++ README.md
@@ -0,0 +1,22 @@
+# Documentation
+
+Basic start with node for Html theme or site
+
+> Be sure to changed the name of the project in `bower.json` and `package.json`
+
+## Requirements
+
+* Node JS
+* Bower `sudo npm install -g bower`
+* Grunt `sudo npm install -g grunt-cli`
+* Sass
+
+## Dev Notes
+
+Setup and build:
+
+    npm install && npm start
+
+Build for Dist
+
+    npm run build
--- gruntfile.js
+++ gruntfile.js
@@ -1,61 +1,117 @@
-﻿/*
+﻿
+/*
 This file in the main entry point for defining grunt tasks and using grunt plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
 
 var sassOptions = {
-    expand: true,
-    cwd: 'scss',
-    src: ["**/*.scss"],
-    dest: "css"
-}
+  expand: true,
+  cwd: 'src/scss',
+  src: ["**/*.scss"],
+  dest: "dist/css"
+};
 
-module.exports = function (grunt) {
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+module.exports = function(grunt) {
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        sass: {
-            compact: {
-                options: {
-                    style: 'compact'
-                },
-                files: [
-                  {
-                      expand: sassOptions.expand,
-                      cwd: sassOptions.cwd,
-                      src: sassOptions.src,
-                      dest: sassOptions.dest,
-                      ext: ".css"
-                  }
-                ]
-            },
-            dist: {
-                options: {
-                    style: 'compressed'
-                },
-                files: [
-                  {
-                      expand: sassOptions.expand,
-                      cwd: sassOptions.cwd,
-                      src: sassOptions.src,
-                      dest: sassOptions.dest,
-                      ext: ".min.css"
-                  }
-                ]
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          hostname: 'localhost',
+          livereload: 35729,
+          open: true,
+          base: {
+            path: 'src',
+            options: {
+              index: 'index.html'
             }
-        },
-        watch: {
-            sass: {
-                files: 'scss/**/*.scss',
-                tasks: ["sass"],
-                options: {
-                    livereload: true
-                }
-            }
+          },
+          keepalive: false
         }
-    });
+      }
+    },
+    wiredep: {
+      dev: {
+        src: [
+          'src/*.html',
+          'src/scss/styles.scss'
+        ],
+        options: {
 
-    grunt.registerTask('default', []);
+        }
+      }
+    },
+    sass: {
+      dev: {
+        options: {
+          style: 'compact'
+        },
+        files: [{
+          expand: sassOptions.expand,
+          cwd: sassOptions.cwd,
+          src: sassOptions.src,
+          dest: "src/css",
+          ext: ".css"
+        }]
+      },
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: [{
+          expand: sassOptions.expand,
+          cwd: sassOptions.cwd,
+          src: sassOptions.src,
+          dest: sassOptions.dest,
+          ext: ".min.css"
+        }]
+      }
+    },
+    concat: {
+      options: {
+        separator: ';',
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      dist: {
+        src: [
+          'src/js/app.js'
+        ],
+        dest: 'dist/js/app.js',
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      dist: {
+        files: {
+          'dist/js/app.min.js': ['dist/js/app.js']
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: 'src/scss/**/*.scss',
+        tasks: ["sass:dev"],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+  grunt.registerTask('start', 'Compile then start a connect web server', function(target) {
+    grunt.task.run([
+      'wiredep',
+      'sass',
+      'connect',
+      'watch'
+    ]);
+  });
 };
